2938f8326a8d503b1b000fcabeeba5ab957221b2
PalmarHealer <myoserhd111@gmail.com>
2025-05-26 00:56:06 +0200

    Update start.sh

--- pelican/start.sh
+++ pelican/start.sh
@@ -5,6 +5,9 @@ if [ -z "$(ls -A /var/www/pelican)" ]; then
     curl -L https://github.com/pelican-dev/panel/releases/latest/download/panel.tar.gz | tar -xzv -C /var/www/pelican
     cd /var/www/pelican
     COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
+    php artisan p:environment:setup
+    chmod -R 755 storage/* bootstrap/cache/
+    chown -R www-data:www-data /var/www/pelican
 fi
 
 php-fpm -D
